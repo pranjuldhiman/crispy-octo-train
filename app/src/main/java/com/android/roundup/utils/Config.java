4fe5309c7c4647cb21198c0eb507eb440ce5406d
biswa <biswaranjan.mhnt92@gmail.com>
2020-06-15 00:01:50 +0530

    OCR,Youtube player

---
+++ app/src/main/java/com/android/roundup/utils/Config.java
@@ -0,0 +1,14 @@
+package com.android.roundup.utils;
+
+/**
+ * Created by echessa on 1/13/17.
+ */
+
+public final class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyCG25IwSEZcJuF5Te7kko9XawkHaEJ48Ws";
+
+}
