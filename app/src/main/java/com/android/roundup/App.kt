8cf58cccaebd5ffe6caceb163ee00f7a18a74f02
biswa <biswaranjan.mhnt92@gmail.com>
2020-06-15 19:07:46 +0530

    intro page, login page ui

---
+++ app/src/main/java/com/android/roundup/App.kt
@@ -0,0 +1,19 @@
+package com.android.roundup
+
+import android.app.Application
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        mSelf = this
+    }
+
+    companion object {
+        private var mSelf: App? = null
+
+        fun self(): App {
+            return mSelf!!
+        }
+    }
+}
---
+++ app/src/main/java/com/android/roundup/utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.android.roundup.utils
+
+object Constants  {
+    val IS_FIRST_TIME = "is_first_time"
+    val APPLICATION_PREFS = "app_preference"
+}
