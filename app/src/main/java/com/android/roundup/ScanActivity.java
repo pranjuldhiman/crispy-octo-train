2c301c454fbda6ff544c3813eb4b6b4dbc5f2c3b
bmohanty <biswaranjan.mhnt92@gmail.com>
2020-06-27 20:02:32 +0530

    Light weight new image cropper added

--- app/src/main/java/com/android/roundup/ScanActivity.java
+++ app/src/main/java/com/android/roundup/ScanActivity.java
@@ -2,14 +2,11 @@ package com.android.roundup;
 
 import android.Manifest;
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
@@ -33,23 +30,16 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.core.content.FileProvider;
 
 import com.android.roundup.resultsactivity.ResultsActivity;
-import com.labters.documentscanner.ImageCropActivity;
-import com.labters.documentscanner.helpers.ScannerConstants;
-//import com.yalantis.ucrop.UCrop;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-
-import static androidx.core.content.FileProvider.getUriForFile;
 
 public class ScanActivity extends AppCompatActivity {
     private static final String TAG = ScanActivity.class.getSimpleName();
-    private static final int THUMBNAIL_SIZE = 500;
     private RelativeLayout mRlSearchView, action_bar;
     private FrameLayout mRlCameraView;
     private TextView mTextView, btn_done;
@@ -61,8 +51,6 @@ public class ScanActivity extends AppCompatActivity {
     private static final int CAMERA_PERMISSION_CODE = 200;
     private static final int READ_EXTERNAL_PERMISSION_CODE = 300;
     private static final int WRITE_CAMERA_PERMISSION_CODE = 400;
-    private boolean isCameraPermission = false;
-    private Uri mImageUri = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -116,6 +104,7 @@ public class ScanActivity extends AppCompatActivity {
         });
 
     }
+
     public static void setWindowFlag(Activity activity, final int bits, boolean on) {
         Window win = activity.getWindow();
         WindowManager.LayoutParams winParams = win.getAttributes();
@@ -126,57 +115,9 @@ public class ScanActivity extends AppCompatActivity {
         }
         win.setAttributes(winParams);
     }
+
     private void startCameraSource() {
-        /*Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-        File photo = null;
-        try {
-            photo = this.createTempFile("picture", ".jpg");
-            photo.delete();
-        } catch (Exception e) {
-        }
-        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-        mImageUri = getUriForFile(ScanActivity.this, "com.android.roundup.provider", photo);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, mImageUri);
-        startActivityForResult(intent, 0);*/
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(getString(R.string.select_picture));
-        builder.setPositiveButton(getString(R.string.browse), new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                dialogInterface.dismiss();
-                Intent intent = new Intent(Intent.ACTION_PICK);
-                intent.setType("image/*");
-                startActivityForResult(intent, 1);
-            }
-        });
-
-        builder.setNegativeButton(getString(R.string.camera), new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-                File photo = null;
-                try {
-                    photo = createTempFile("picture", ".jpg");
-                    photo.delete();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-                mImageUri = getUriForFile(ScanActivity.this, "com.android.roundup.provider", photo);
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, mImageUri);
-                startActivityForResult(intent, 0);
-            }
-        });
-
-        builder.setNeutralButton(getString(R.string.cancel), new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                dialogInterface.dismiss();
-            }
-        });
-        AlertDialog dialog = builder.create();
-        dialog.show();
+        CropImage.activity(null).setGuidelines(CropImageView.Guidelines.ON).start(this);
     }
 
     private File createTempFile(String picture, String s) throws Exception {
@@ -201,56 +142,21 @@ public class ScanActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         switch (requestCode) {
-            case 0:
-                if (resultCode == Activity.RESULT_OK) {
-                   /* UCrop ucrop = UCrop.of(mImageUri, Uri.fromFile(new File(ScanActivity.this.getCacheDir(), "CropImage.jpg")))
-                            .withAspectRatio(1, 1)
-                            .withMaxResultSize(1000, 1000);
-                    ucrop = advancedConfig(ucrop);
-                    ucrop.start(this, UCrop.REQUEST_CROP);*/
-
-                    try {
-                        ScannerConstants.selectedImageBitmap = MediaStore.Images.Media.getBitmap(
-                                this.getContentResolver(), mImageUri);
-                        startActivityForResult(new Intent(ScanActivity.this, ImageCropActivity.class), 1234);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-                break;
-
-            case 1:
-                assert data != null;
-                Uri selectedImage = data.getData();
-                Bitmap bitmap;
-                try {
-                    if (selectedImage != null){
-                        InputStream inputStream = getContentResolver().openInputStream(selectedImage);
-                        bitmap = BitmapFactory.decodeStream(inputStream);
-                        ScannerConstants.selectedImageBitmap = bitmap;
-                        startActivityForResult(new Intent(this, ImageCropActivity.class),1234);
-                    }
-                } catch (Exception e ) {
-                    e.printStackTrace();
-                }
-                break;
-           /* case UCrop.REQUEST_CROP:
-                final Uri mImageUri = UCrop.getOutput(data);
-                String imagePath = getRealPathFromURIPath(mImageUri, this);
-                startActivity(new Intent(ScanActivity.this, PreviewActivity.class).putExtra("imagePath", imagePath));
-                break;*/
-            case 1234:
-                if (ScannerConstants.selectedImageBitmap != null) {
-
+            case CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE:
+                CropImage.ActivityResult result = CropImage.getActivityResult(data);
+                if (resultCode == RESULT_OK) {
                     startActivity(new Intent(ScanActivity.this, ResultsActivity.class)
-                            .putExtra("imagePath", getRealPathFromURIPath(getImageUri(this, ScannerConstants.selectedImageBitmap), this)));
-
-                } else
-                    Toast.makeText(ScanActivity.this, "Not OK", Toast.LENGTH_LONG).show();
+                            .putExtra("imagePath", getRealPathFromURIPath(result.getUri(), this)));
+                    Toast.makeText(this, "Cropping successful, Sample: " + result.getSampleSize(), Toast.LENGTH_LONG).show();
+                } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                    Toast.makeText(this, "Cropping failed: " + result.getError(), Toast.LENGTH_LONG).show();
+                }
                 break;
+            default:
+                break;
+
         }
     }
-
 
     private Uri getImageUri(Context context, Bitmap inImage) {
         ByteArrayOutputStream bytes = new ByteArrayOutputStream();
@@ -258,11 +164,6 @@ public class ScanActivity extends AppCompatActivity {
         String path = MediaStore.Images.Media.insertImage(context.getContentResolver(), inImage, "abc", null);
         return Uri.parse(path);
     }
-   /* private UCrop advancedConfig(UCrop ucrop) {
-        UCrop.Options options = new UCrop.Options();
-        options.setFreeStyleCropEnabled(true);
-        return ucrop.withOptions(options);
-    }*/
 
     private String getRealPathFromURIPath(Uri contentURI, Context activity) {
         Cursor cursor = activity.getContentResolver().query(contentURI, null, null, null, null);
