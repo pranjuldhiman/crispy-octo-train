2477a13f1cf3f94f7138580297f397870595dbfa
Ankita Mehra <ankita.mehra@softradix.in>
2022-02-23 13:27:51 +0530

    wats app crash resolved

--- app/src/main/java/com/rs/roundupclasses/dashboard/MainActivity.kt
+++ app/src/main/java/com/rs/roundupclasses/dashboard/MainActivity.kt
@@ -8,6 +8,7 @@ import android.content.Intent
 import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.graphics.Color
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.telephony.PhoneNumberUtils
@@ -29,13 +30,11 @@ import com.rs.roundupclasses.R
 import com.rs.roundupclasses.chooseicon.ChooseIconActivity
 import com.rs.roundupclasses.dashboard.adapter.*
 import com.rs.roundupclasses.dashboard.model.Subcategory
-import com.rs.roundupclasses.examcontent.DemoActivity
 import com.rs.roundupclasses.examcontent.ExamContentActivity
 import com.rs.roundupclasses.library.LibraryActivity
 import com.rs.roundupclasses.notify.NotificationActivity
 import com.rs.roundupclasses.profile.ProfileActivity
 import com.rs.roundupclasses.scan.ScanActivity
-import com.rs.roundupclasses.subjectlist.SubjectActivity
 import com.rs.roundupclasses.utils.ApiStatus
 import com.rs.roundupclasses.utils.Constants
 import com.rs.roundupclasses.utils.ProgressDialog
@@ -46,13 +45,14 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
-    lateinit  var progress:ProgressDialog;
+    lateinit var progress: ProgressDialog;
     lateinit var sharedPreferences: SharedPreferences
     lateinit var type: String
     lateinit var catid: String
 
     private val viewModel by lazy {
-        ViewModelProviders.of(this, RoundUpHelper.viewModelFactory { MainViewModel(this) }).get(MainViewModel::class.java)
+        ViewModelProviders.of(this, RoundUpHelper.viewModelFactory { MainViewModel(this) })
+            .get(MainViewModel::class.java)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -69,16 +69,17 @@ class MainActivity : AppCompatActivity() {
             }
         }
         super.onCreate(savedInstanceState)
-        sharedPreferences = this.getSharedPreferences(Constants.SHAREDPREFERENCEFILE, Context.MODE_PRIVATE)
-        var userid: String= sharedPreferences.getString(Constants.USERID,"").toString()
-        Log.e("SHAREDPREFERENCE","userid is....."+userid)
+        sharedPreferences =
+            this.getSharedPreferences(Constants.SHAREDPREFERENCEFILE, Context.MODE_PRIVATE)
+        var userid: String = sharedPreferences.getString(Constants.USERID, "").toString()
+        Log.e("SHAREDPREFERENCE", "userid is....." + userid)
         setContentView(R.layout.activity_main)
         progress = ProgressDialog(this)
         configureUi()
-     //   loadViewPager()
+        //   loadViewPager()
         getDashboardData(userid)
         setViewModelObservers()
-        distance(30.7387,76.7807,30.7088,76.8384)
+        distance(30.7387, 76.7807, 30.7088, 76.8384)
 
         viewModel.status.observe(this, Observer {
             when (it) {
@@ -112,7 +113,7 @@ class MainActivity : AppCompatActivity() {
         dist = Math.acos(dist)
         dist = rad2deg(dist)
         dist = dist * 60 * 1.1515
-        Log.e("LATLONG","distance is......"+dist)
+        Log.e("LATLONG", "distance is......" + dist)
 
         return dist
     }
@@ -126,36 +127,33 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-
-    private fun getDashboardData(userid:String) {
+    private fun getDashboardData(userid: String) {
         viewModel.getDashboardData(userid)
     }
 
     private fun setViewModelObservers() {
         viewModel.apply {
             listOfSearchResult.observe(this@MainActivity, Observer {
-                var value:Boolean = true
+                var value: Boolean = true
 
-                for(data in it!!.indices)
-                {
+                for (data in it!!.indices) {
 
-                    if(it.get(data).subcategory!!.size>0)
-                    {
-                        for(datas in it.get(data).subcategory!!.indices)
-                        {
-                            catid =   it.get(data).subcategory!!.get(datas).id.toString()
-                            type =   it.get(data).subcategory!!.get(datas).name.toString()
+                    if (it.get(data).subcategory!!.size > 0) {
+                        for (datas in it.get(data).subcategory!!.indices) {
+                            catid = it.get(data).subcategory!!.get(datas).id.toString()
+                            type = it.get(data).subcategory!!.get(datas).name.toString()
                             break
                         }
-                        Log.e("CATEGORYNAME","categoryname name name name is......"+type)
-                        Log.e("CATEGORYNAME","categoryname id id id id is......"+catid)
+                        Log.e("CATEGORYNAME", "categoryname name name name is......" + type)
+                        Log.e("CATEGORYNAME", "categoryname id id id id is......" + catid)
 
-                        Log.e("CATEGORYNAME","categoryname is......"+it.get(data).category)
+                        Log.e("CATEGORYNAME", "categoryname is......" + it.get(data).category)
                         break
-                    }
-                    else
-                    {
-                        Log.e("CATEGORYNAME","categoryname else else is......"+it.get(data).category)
+                    } else {
+                        Log.e(
+                            "CATEGORYNAME",
+                            "categoryname else else is......" + it.get(data).category
+                        )
                     }
                 }
 
@@ -165,17 +163,15 @@ class MainActivity : AppCompatActivity() {
                 rcv_dashboard.apply {
                     layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
                     adapter = DashBoardCategoryAdapter({
-                        if(it.having_subcategory.equals("1"))
-                        {
-                            var list:ArrayList<Subcategory> = it.subcategory as ArrayList<Subcategory>
+                        if (it.having_subcategory.equals("1")) {
+                            var list: ArrayList<Subcategory> =
+                                it.subcategory as ArrayList<Subcategory>
                             val intent = Intent(context, ChooseIconActivity::class.java)
                             intent.putExtra("type", it.category)
                             intent.putExtra("LIST", list)
                             startActivity(intent)
-                        }
-                        else
-                        {
-                           sharedPreferences.edit { putString(Constants.TOPICID,"ONE") }
+                        } else {
+                            sharedPreferences.edit { putString(Constants.TOPICID, "ONE") }
                             val intent = Intent(context, ExamContentActivity::class.java)
                             intent.putExtra("type", it.category)
                             context.startActivity(intent)
@@ -187,17 +183,17 @@ class MainActivity : AppCompatActivity() {
 
             bannerResult.observe(this@MainActivity, Observer {
                 val viewPager = findViewById<View>(R.id.viewPager) as ViewPager
-                val viewPagerAdapter = AdsAdapter(this@MainActivity,it!!)
+                val viewPagerAdapter = AdsAdapter(this@MainActivity, it!!)
                 viewPager.adapter = viewPagerAdapter
             })
 
             appid.observe(this@MainActivity, Observer {
-                sharedPreferences.edit { putString(Constants.APPID,it.toString()) }
+                sharedPreferences.edit { putString(Constants.APPID, it.toString()) }
 
             })
 
             appkey.observe(this@MainActivity, Observer {
-                sharedPreferences.edit { putString(Constants.APPKEY,it.toString()) }
+                sharedPreferences.edit { putString(Constants.APPKEY, it.toString()) }
 
             })
 
@@ -224,12 +220,23 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun isAppInstalled(packageName: String): Boolean {
+        val pm = packageManager
+        val app_installed: Boolean = try {
+            pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES)
+            true
+        } catch (e: PackageManager.NameNotFoundException) {
+            false
+        }
+        return app_installed
+    }
+
     private fun configureUi() {
         add_photo_layout.setOnClickListener {
             startActivity(Intent(this, ScanActivity::class.java))
         }
 
-        aboutus.setOnClickListener{
+        aboutus.setOnClickListener {
             val intent = Intent(this@MainActivity, WebViewActivity::class.java)
             intent.putExtra("type", "About Us")
             intent.putExtra("URL", "http://roundupclasses.com/about-us.html")
@@ -237,9 +244,9 @@ class MainActivity : AppCompatActivity() {
         }
 
         feedback.setOnClickListener {
-           /* val intent = Intent(this@MainActivity, WebViewActivity::class.java)
-            intent.putExtra("URL", "https://trainingscholar.com/studyapp/feedback.html")
-            startActivity(intent)*/
+            /* val intent = Intent(this@MainActivity, WebViewActivity::class.java)
+             intent.putExtra("URL", "https://trainingscholar.com/studyapp/feedback.html")
+             startActivity(intent)*/
             startActivity(Intent(this, LibraryActivity::class.java))
         }
 
@@ -248,64 +255,62 @@ class MainActivity : AppCompatActivity() {
         }
 
         rcv_cms_tabs.apply {
-            layoutManager = GridLayoutManager(context,4)
+            layoutManager = GridLayoutManager(context, 4)
             adapter = CMSAdapter({
 
-                if(it==0)
-                {
+                if (it == 0) {
 
-                    sharedPreferences.edit { putString(Constants.TOPICID,"ONE") }
+                    sharedPreferences.edit { putString(Constants.TOPICID, "ONE") }
 
                     val intent = Intent(context, ExamContentActivity::class.java)
                     intent.putExtra("type", "Test")
                     context.startActivity(intent)
 
-                   /* val intent = Intent(context, SubjectActivity::class.java)
-                    intent.putExtra("type", type.toString())
-                    intent.putExtra("ID", catid.toString())
-                    context.startActivity(intent)*/
-                }
-               else if(it==1)
-                {
+                    /* val intent = Intent(context, SubjectActivity::class.java)
+                     intent.putExtra("type", type.toString())
+                     intent.putExtra("ID", catid.toString())
+                     context.startActivity(intent)*/
+                } else if (it == 1) {
                     startActivity(Intent(context, LibraryActivity::class.java))
-          //          val intent = Intent(context, VideoActivity::class.java)
-              //      context.startActivity(intent)
-                }
-                else if(it==2)
-                {
-                    Log.e("CHECKDATA","whatsapp is called called")
-                    var message = ""
-                //    var formattedNumber = "+918727888793"
-                 //   var formattedNumber = "+917347444327"
+                    //          val intent = Intent(context, VideoActivity::class.java)
+                    //      context.startActivity(intent)
+                } else if (it == 2) {
+                    Log.e("CHECKDATA", "whatsapp is called called")
+
                     var formattedNumber = "+919877163246"
-                    formattedNumber = formattedNumber.replace(" ", "").replace("+", "")
-                    val sendIntent = Intent("android.intent.action.MAIN")
-                    sendIntent.type = "text/plain"
-                    sendIntent.putExtra(Intent.EXTRA_TEXT, message)
-                    sendIntent.component = ComponentName(
-                        "com.whatsapp",
-                        "com.whatsapp.Conversation"
-                    )
-                    sendIntent.putExtra(
-                        "jid",
-                        PhoneNumberUtils.stripSeparators(formattedNumber) + "@s.whatsapp.net"
-                    )
-                    context.startActivity(sendIntent)
-                }
-                else if(it==3)
-                {
+                    if (isAppInstalled("com.whatsapp")) {
+                        var message = ""
+                        //    var formattedNumber = "+918727888793"
+                        //   var formattedNumber = "+917347444327"
+                        formattedNumber = formattedNumber.replace(" ", "").replace("+", "")
+                        val sendIntent = Intent("android.intent.action.MAIN")
+                        sendIntent.type = "text/plain"
+                        sendIntent.putExtra(Intent.EXTRA_TEXT, message)
+                        sendIntent.component = ComponentName(
+                            "com.whatsapp",
+                            "com.whatsapp.Conversation"
+                        )
+                        sendIntent.putExtra(
+                            "jid",
+                            PhoneNumberUtils.stripSeparators(formattedNumber) + "@s.whatsapp.net"
+                        )
+                        context.startActivity(sendIntent)
+                    } else {
+                        val uri: Uri =
+                            Uri.parse("https://api.whatsapp.com/send?phone=$formattedNumber&text=hii!")
+                        val sendIntent = Intent(Intent.ACTION_VIEW, uri)
+                        context.startActivity(sendIntent)
+                    }
+
+                } else if (it == 3) {
                     val intent = Intent(context, NotificationActivity::class.java)
                     startActivity(intent)
-                }
-                else if(it==4)
-                {
+                } else if (it == 4) {
                     val intent = Intent(context, WebViewActivity::class.java)
                     intent.putExtra("type", "About Us")
                     intent.putExtra("URL", "http://roundupclasses.com/about-us.html")
                     startActivity(intent)
-                }
-                else if(it==5)
-                {
+                } else if (it == 5) {
                     val sendIntent = Intent()
                     sendIntent.action = Intent.ACTION_SEND
                     sendIntent.putExtra(
@@ -316,29 +321,25 @@ class MainActivity : AppCompatActivity() {
                     //AIzaSyDLqJSj1aoL7kLF44jUkB9GoEoYyn68u3U
                     sendIntent.type = "text/plain"
                     startActivity(sendIntent)
-                }
-                else if(it==6)
-                {
+                } else if (it == 6) {
 
                     askForPermission(
                         Manifest.permission.CAMERA,
                         CAMERA_PERMISSION_CODE
                     )
 
-                 /*   val intent = Intent(context, Feedback::class.java)
-                 //   intent.putExtra("type", "FeedBack")
-                  //  intent.putExtra("URL", "http://roundupclasses.com/feedback.html")
-                    startActivity(intent)*/
-                }
-                else if(it==7)
-                {
-                     val intent = Intent(context, WebViewActivity::class.java)
+                    /*   val intent = Intent(context, Feedback::class.java)
+                    //   intent.putExtra("type", "FeedBack")
+                     //  intent.putExtra("URL", "http://roundupclasses.com/feedback.html")
+                       startActivity(intent)*/
+                } else if (it == 7) {
+                    val intent = Intent(context, WebViewActivity::class.java)
                     intent.putExtra("type", "Contact Us")
                     intent.putExtra("URL", "http://roundupclasses.com/contact-us.html")
                     startActivity(intent)
                 }
-            },context)
-           // addItemDecoration(GridDividerItemDecoration(0))
+            }, context)
+            // addItemDecoration(GridDividerItemDecoration(0))
         }
 
         contentVP.apply {
@@ -381,10 +382,10 @@ class MainActivity : AppCompatActivity() {
                         )
                         == PackageManager.PERMISSION_GRANTED
                     ) {
-                         val intent = Intent(this, Feedback::class.java)
-              //   intent.putExtra("type", "FeedBack")
-               //  intent.putExtra("URL", "http://roundupclasses.com/feedback.html")
-                 startActivity(intent)
+                        val intent = Intent(this, Feedback::class.java)
+                        //   intent.putExtra("type", "FeedBack")
+                        //  intent.putExtra("URL", "http://roundupclasses.com/feedback.html")
+                        startActivity(intent)
                     } else {
                         askForPermission(
                             Manifest.permission.WRITE_EXTERNAL_STORAGE,
@@ -432,8 +433,8 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-    fun startCameraSource()
-    {
+
+    fun startCameraSource() {
         val intent = Intent(this, Feedback::class.java)
         startActivity(intent)
     }
@@ -458,7 +459,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun loadViewPager() {
         val viewPager = findViewById<View>(R.id.viewPager) as ViewPager
-     //   val viewPagerAdapter = AdsAdapter(this)
-      //  viewPager.adapter = viewPagerAdapter
+        //   val viewPagerAdapter = AdsAdapter(this)
+        //  viewPager.adapter = viewPagerAdapter
     }
 }
